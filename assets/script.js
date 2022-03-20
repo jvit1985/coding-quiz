40d75810441f5689c59f00e7d13b8f2f341db283
Justin Vititoe <jvit1985@yahoo.com>
2022-03-20 16:32:46 -0500

    added function to only start timer when start quiz button is clicked

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,76 @@
 var timerEl = document.getElementById("countdown-timer-view");
-var mainEl = document.getElementById("question-display");
+var displayEl = document.getElementById("display-question");
+var mainQuestionEl = document.getElementById("initial-rules");
+var btn = document.getElementById("button1");
 
-var allQuestions = 
-"put questions here"
-var questions = message.split("?");
+displayEl.textContent="Coding Quiz Challenge";
+mainQuestionEl.textContent="Answer the questions as quickly as possible before the timer expires, but keep in mind wrong answers will deduct 10 seconds from your time/score."
 
-function countdown() {
-    var timeLeft = 100;
+var questions = [
+    {
+        question: "Question 1 here",
+        optionA: "Answer 1 here",
+        optionB: "Answer 2 here",
+        optionC: "Correct Answer Here",
+        optionD: "Answer 4 here",
+        correctionOption: "optionC"
+    },
 
-    var timeInterval = setInterval(function() {
-        if (timeLeft > 0) {
+    {
+        question: "Question 2 here",
+        optionA: "Correct Answer Here",
+        optionB: "Answer 2 here",
+        optionC: "Answer 3 here",
+        optionD: "Answer 4 here",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Question 3 here",
+        optionA: "Answer 1 here",
+        optionB: "Answer 2 here",
+        optionC: "Answer 3 here",
+        optionD: "Correct Answer Here",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Question 4 here",
+        optionA: "Answer 1 here",
+        optionB: "Answer 2 here",
+        optionC: "Correct Answer Here",
+        optionD: "Answer 4 here",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Question 5 here",
+        optionA: "Answer 1 here",
+        optionB: "Correct Answer Here",
+        optionC: "Answer 3 here",
+        optionD: "Answer 4 here",
+        correctOption: "optionB"
+    }
+];
+
+btn.addEventListener("click", startGame); 
+
+function startGame() {
+
+    function countdown() {
+        var timeLeft = 100;
+
+        var timeInterval = setInterval(function() {
+            if (timeLeft > 0) {
             timerEl.textContent = timeLeft;
             timeLeft--;
-        }
-        else {
+            }
+            else {
             clearInterval(timeInterval);
-            timerEl.textContent ='';
-            displayQuestion();
-        }
-    }, 1000);
-}
+            timerEl.textContent = 0;
+            }
+        }, 1000);
+    };
+    countdown();
+};
 
-function displayQuestion() {
-    var question = 0;
-
-    var questionInterval = setInterval(function() {
-        if (questions[question] === undefined) {
-            clearInterval(questionInterval);
-        }
-        else {
-            mainEl.textContent = questions[question];
-            question++;
-        }
-    });
-}
-
-countdown();
